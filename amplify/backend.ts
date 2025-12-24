ace54649324d87484723708d93df9e252ed5d170
Chung Hoon Hong <datahong@umich.edu>
2025-12-23 17:51:36 -0800

    chore: configure storage browser with existing bucket acs-artifacts-temporary-beta-us-west-2

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -22,18 +22,18 @@ const backend = defineBackend({
  *
  * Note: Ensure the bucket exists before deploying this code, as it only sets up IAM policies and does not create the S3 bucket.
  */
-const customBucketName = "my-existing-bucket";
+const customBucketName = "acs-artifacts-temporary-beta-us-west-2";
 
 backend.addOutput({
   version: "1.3",
   storage: {
-    aws_region: "us-east-1",
+    aws_region: "us-west-2",
     bucket_name: customBucketName,
     buckets: [
       {
         name: customBucketName,
         bucket_name: customBucketName,
-        aws_region: "us-east-1",
+        aws_region: "us-west-2",
         //@ts-expect-error amplify backend type issue https://github.com/aws-amplify/amplify-backend/issues/2569
         paths: {
           "public/*": {
